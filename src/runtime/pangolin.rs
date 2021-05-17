e04126225ec53bc82dd2e9e3bf849c928a52c171
Xavier Lau <c.estlavie@icloud.com>
2021-05-18 01:20:55 +0800

    backup

--- src/runtime/pangolin.rs
+++ src/runtime/pangolin.rs
@@ -14,55 +14,55 @@ pub type Balance = u128;
 
 #[derive(Debug, Decode)]
 pub enum Event {
-	#[codec(index = "0")]
+	#[codec(index = 0)]
 	System(system::Event<AccountId>),
-	#[codec(index = "4")]
+	#[codec(index = 4)]
 	Balances(balances::Event<AccountId, Balance>),
-	#[codec(index = "5")]
+	#[codec(index = 5)]
 	Kton(balances::Event<AccountId, Balance>),
-	#[codec(index = "8")]
+	#[codec(index = 8)]
 	Staking(staking::Event<BlockNumber, AccountId, Balance>),
-	#[codec(index = "9")]
+	#[codec(index = 9)]
 	Offences(offences::Event),
-	#[codec(index = "11")]
+	#[codec(index = 11)]
 	Session(session::Event),
-	#[codec(index = "13")]
+	#[codec(index = 13)]
 	Grandpa(grandpa::Event<AccountId>),
-	#[codec(index = "14")]
+	#[codec(index = 14)]
 	ImOnline(im_online::Event<AccountId, staking::Exposure<AccountId, Balance>>),
-	#[codec(index = "16")]
+	#[codec(index = 16)]
 	Democracy(democracy::Event<BlockNumber, Hash, AccountId, Balance>),
-	#[codec(index = "17")]
+	#[codec(index = 17)]
 	Council(collective::Event<Hash, AccountId>),
-	#[codec(index = "18")]
+	#[codec(index = 18)]
 	TechnicalCommittee(collective::Event<Hash, AccountId>),
-	#[codec(index = "19")]
+	#[codec(index = 19)]
 	ElectionsPhragmen(elections_phragmen::Event<AccountId, Balance>),
-	#[codec(index = "20")]
+	#[codec(index = 20)]
 	TechnicalMembership(membership::Event),
-	#[codec(index = "21")]
+	#[codec(index = 21)]
 	Treasury(treasury::Event<Hash, AccountId, Balance>),
-	#[codec(index = "22")]
+	#[codec(index = 22)]
 	Claims(claims::Event<AccountId, Balance>),
-	#[codec(index = "23")]
+	#[codec(index = 23)]
 	Vesting(vesting::Event<AccountId, Balance>),
-	#[codec(index = "24")]
+	#[codec(index = 24)]
 	Scheduler(scheduler::Event<BlockNumber>),
-	#[codec(index = "30")]
+	#[codec(index = 30)]
 	Proxy(proxy::Event<Hash, AccountId, ProxyType>),
-	#[codec(index = "31")]
+	#[codec(index = 31)]
 	Sudo(sudo::Event<AccountId>),
-	#[codec(index = "32")]
+	#[codec(index = 32)]
 	CrabIssuing(crab_issuing::Event<AccountId, Balance>),
-	#[codec(index = "35")]
+	#[codec(index = 35)]
 	EthereumRelay(ethereum_relay::Event<AccountId>),
-	#[codec(index = "36")]
+	#[codec(index = 36)]
 	EthereumBacking(ethereum_backing::Event<AccountId, Balance>),
-	#[codec(index = "39")]
+	#[codec(index = 39)]
 	EVM(dvm::Event<AccountId>),
-	#[codec(index = "40")]
+	#[codec(index = 40)]
 	Ethereum(ethereum::Event),
-	#[codec(index = "41")]
+	#[codec(index = 41)]
 	EthereumRelayAuthorities(relay_authorities::Event<BlockNumber, AccountId>),
 }
 
